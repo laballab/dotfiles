f283e8a736941a0b3cfead90c0c002e6f5e85176
Luis Ballabeni <lballabe@cisco.com>
2020-03-09 23:51:19 -0400

    fixed it

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,10 @@
 function fish_prompt --description 'Write out the prompt'
+	if not set -q __git_cb
+    		set __git_cb (set_color cyan)":"(git branch ^/dev/null | grep \* | sed 's/* //')
+    	end
 	if test -z $WINDOW
-        printf '%s%s@%s%s%s%s%s> ' (set_color yellow) (whoami) (set_color purple) (echo -n 'MBP') (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+        printf '%s%s%s%s%s%s%s%s%s%s%s' (set_color yellow) (whoami) (echo -n '@') (set_color purple) (echo -n 'MBP') (set_color $fish_color_cwd) (prompt_pwd) $__git_cb (set_color --bold 585A65) (echo -n '>>') (set_color normal)
     else
-        printf '%s%s@%s%s%s(%s)%s%s%s> ' (set_color yellow) (whoami) (set_color purple) (echo -n 'MBP') (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+        printf '%s%s%s%s%s%s%s(%s)%s%s%s%s> ' (set_color yellow) (whoami) (set_color brwhite) (echo -n '@') (set_color purple) (echo -n 'MBP') (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) $__git_cb (set_color normal)
     end
 end

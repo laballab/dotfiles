d4a57829d2e40ecd22e9bc58a78286af8799d674
Luis Ballabeni <luis.ballabeni@lexisnexis.com>
2021-08-14 03:40:26 -0400

    updated fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,16 @@
 function fish_prompt --description 'Write out the prompt'
-	if not set -q __git_cb
-    		set __git_cb (set_color cyan)":"(git branch ^/dev/null | grep \* | sed 's/* //')
-    	end
-	if test -z $WINDOW
-        printf '%s%s%s%s%s%s%s%s%s%s%s' (set_color yellow) (whoami) (echo -n '@') (set_color purple) (echo -n 'MBP') (set_color $fish_color_cwd) (prompt_pwd) $__git_cb (set_color --bold 585A65) (echo -n '>>') (set_color normal)
-    else
-        printf '%s%s%s%s%s%s%s(%s)%s%s%s%s> ' (set_color yellow) (whoami) (set_color brwhite) (echo -n '@') (set_color purple) (echo -n 'MBP') (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) $__git_cb (set_color normal)
-    end
+  set -g __fish_git_prompt_showdirtystate
+  set -g __fish_git_prompt_use_informative_chars
+
+  set -l last_status $status
+  set -l last_status_color "green"
+
+  if test $last_status -ne 0
+    set last_status_color "red"
+  end
+
+  echo -s -e (set_color yellow) (whoami) " " \
+    (set_color $fish_color_cwd) (prompt_pwd) \
+    (set_color cyan) (__fish_git_prompt) \
+    (set_color $last_status_color) "\n["$last_status"]" (set_color --bold 585A65) " >> "
 end
